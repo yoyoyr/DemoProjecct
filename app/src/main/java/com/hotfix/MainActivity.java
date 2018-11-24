806f0735692064146f4a8b8251319865c30cabf6
yoyo <296550494@qq.com>
2018-11-24 22:30:21 +0800

    第一个版本提交

---
+++ app/src/main/java/com/hotfix/MainActivity.java
@@ -0,0 +1,38 @@
+package com.hotfix;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
+
+import com.tinker.TinkerManager;
+
+import java.io.File;
+
+public class MainActivity extends AppCompatActivity {
+    private String mPath;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mPath = getExternalCacheDir().getAbsolutePath() + File.separatorChar + "patch_signed.apk";
+    }
+
+    /**
+     * 加载Tinker补丁
+     *
+     * @param view
+     */
+    public void hotFix(View view) {
+        File patchFile = new File(mPath);
+        if (patchFile.exists()) {
+            TinkerManager.loadPatch(mPath);
+            Toast.makeText(this, "File Exists,Please wait a moment ", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "File No Exists", Toast.LENGTH_SHORT).show();
+        }
+    }
+}
+
